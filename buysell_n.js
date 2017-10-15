b62a131e45d6f683e9eba8feba44734c27fbd6b5
Jo Pearce <jo@jopearce.co.uk>
2017-10-15 08:56:15 +0100

    Modified the buy/sell n bruteforce method to work with unmatched buys & sells.

--- buysell_n.js
+++ buysell_n.js
@@ -3,22 +3,37 @@ function bruteforce(prices, n) {
         return 0;
     }
 
+    // This alternates the cost of each transaction between buy and sell (starting with buy)
+    let sumTrades = (total, current, index) => {
+        return (index % 2 === 0)? total - prices[current] : total + prices[current];
+    };
+
+
+    // Fill up the matrix with the cost of transacting n-times on day zero.
     let T = [];
     for (let i = 0; i < n + 1; i++) {
-        let emptyTrades = Array(i).fill([0, 0]);
-        T[i] = Array(prices.length).fill({ trades: Array.from(emptyTrades), value: 0 });
+        let zeroTrades = Array(i).fill(0);
+        let zeroValue = zeroTrades.reduce(sumTrades, 0);
+        T[i] = Array(prices.length).fill({ trades: Array.from(zeroTrades), value: zeroValue });
     }
 
 
     for (let i = 1; i < n + 1; i++) {
-        for (let j = 1; j < prices.length; j++) {
-            let maxVal = { trades: Array.from(T[i][0].trades), value: 0 };
+        for (let j = 1; j < prices.length; j++) {  
+
+            // We insert into the matrix the max of 
+            // -  selling/buying today or
+            // - not transacting today at all (T[i][j-1])
+            // if we buy/sell today then we must check the max
+            // of each day where we've transacted only (i-1) times.
+
+            let maxVal = { trades: Array.from(T[i][0].trades), value: T[i][0].value };
             for (let m = 0; m < j; m++) {
-                let thisProfit = prices[j] - prices[m] + T[i - 1][m].value;
+                let trades = Array.from(T[i-1][m].trades);
+                trades.push(j);
+                let thisProfit = trades.reduce(sumTrades, 0);
                 if (thisProfit > maxVal.value) {
-                    maxVal.trades.length = 0;
-                    maxVal.trades.push(...T[i-1][m].trades);
-                    maxVal.trades.push([m, j]);
+                    maxVal.trades = trades;
                     maxVal.value = thisProfit;
                 }
             }
@@ -26,8 +41,8 @@ function bruteforce(prices, n) {
         }
     }
 
-    //console.log(T);
-    //console.log(T[n][prices.length - 1]);
+    // console.log(T[n]);
+    // console.log(T[n][prices.length - 1]);
     return T[n][prices.length - 1];
 }
 
--- spec/buysell_n.spec.js
+++ spec/buysell_n.spec.js
@@ -3,16 +3,24 @@ const buysell = require('../buysell_n');
 
 describe('buysell n times', () => {
 
-    // Where n is the number of full buy-then-sell transactions.
+    // Where n is the number of transactions (alternating buy then sell)
 
     const testcases = [
         {
             input: [[1, 2, 1, 2, 3, 2, 3, 2], 4],
-            output: { trades: [ [ 0, 0 ], [ 0, 1 ], [ 2, 4 ], [ 5, 6 ] ], value: 4 }
+            output: { trades: [ 0, 1, 2, 4 ], value: 3 }
         },
         {
             input: [[1, 2, 3, 4, 5, 1, 2, 3], 4],
-            output: { trades: [ [ 0, 0 ], [ 0, 0 ], [ 0, 4 ], [ 5, 7 ] ], value: 6 }
+            output: { trades: [ 0, 4, 5, 7 ], value: 6 }
+        },
+        {
+            input: [[1, 2, 3, 4, 5, 1, 2, 3], 3],
+            output: { trades: [ 0, 4, 5 ], value: 3 }
+        },
+        {
+            input: [[1, 1, 1, 5, 6, 7], 3],
+            output: { trades: [ 0, 0, 0 ], value: -1 }
         }
     ]
 
